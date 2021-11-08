52819f23475b63e51093c539a1cbb7767a95bbcd
949027 <949027@gmail.com>
2021-11-08 17:39:10 +0700

    Separated functions

--- salary.py
+++ salary.py
@@ -1,20 +1,23 @@
-import requests
+import os
+
+from dotenv import load_dotenv
 from terminaltables import AsciiTable
+import requests
 
 
 def predict_salary(salary_from, salary_to):
     if salary_from and salary_to:
         avg_salary = (salary_from + salary_to) / 2
-        return int(avg_salary)
+        return avg_salary
 
     elif not salary_from and not salary_to:
         return None
 
     elif salary_from:
-        return int(salary_from * 1.2)
+        return salary_from * 1.2
 
     elif salary_to:
-        return int(salary_to * 0.8)
+        return salary_to * 0.8
 
 
 def predict_rub_salary_hh(language):
@@ -37,91 +40,100 @@ def predict_rub_salary_hh(language):
         for vacancy in vacancies:
             found_vacancies += 1
             salary = vacancy['salary']
-            if salary:
-                if salary['currency'] == 'RUR':
-                    salary = predict_salary(salary['from'], salary['to'])
-                    sum_salary += salary
-                    processed_vacancies +=1
+            if salary and salary['currency'] == 'RUR':
+                salary = predict_salary(salary['from'], salary['to'])
+                sum_salary += salary
+                processed_vacancies += 1
         page += 1
 
     average_solary = int(sum_salary / processed_vacancies)
 
     statistics = {
-        'vacancies_found': found_vacancies,
-        'vacancies_processed': processed_vacancies,
+        'found_vacancies': found_vacancies,
+        'processed_vacancies': processed_vacancies,
         'average_solary': average_solary,
     }
     return statistics
 
 
-def predict_rub_salary_sj(language):
-    result_more = True
+def predict_rub_salary_sj(language, token):
+    results_more = True
     page = 0
     found_vacancies = 0
     sum_salary = 0
     processed_vacancies = 0
-    while result_more:
-        page += 1
+
+    while results_more:
         url = 'https://api.superjob.ru/2.0/vacancies/'
-        headers = {
-            'X-Api-App-Id': 'v3.r.135478234.45de286f7bd7826e6a6d84180f47cb92aa81497f.f60797102d049007d32304a50b1d38f44c56b102'
-        }
+        headers = {'X-Api-App-Id': token}
         payload = {
             't': '4',
             'keyword': language,
             'page': page,
-            'count': 50,
+            'count': 100,
             'period': 0,
         }
 
         page_response = requests.get(url, headers=headers, params=payload)
         page_response.raise_for_status()
         vacancies = page_response.json()['objects']
-        result_more = page_response.json()['more']
+        results_more = page_response.json()['more']
 
         for vacancy in vacancies:
             found_vacancies += 1
             if vacancy['currency'] == 'rub':
-                salary = predict_salary(vacancy['payment_from'], vacancy['payment_to'])
+                salary = predict_salary(
+                    vacancy['payment_from'],
+                    vacancy['payment_to'],
+                )
                 if salary:
                     sum_salary += salary
                     processed_vacancies += 1
 
+        page += 1
+
     average_solary = int(sum_salary / processed_vacancies)
 
     statistics = {
-        'vacancies_found': found_vacancies,
-        'vacancies_processed': processed_vacancies,
+        'found_vacancies': found_vacancies,
+        'processed_vacancies': processed_vacancies,
         'average_solary': average_solary,
     }
+
     return statistics
 
 
-# def create_table(statistics):
-#     table_data = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
-#     table_data.append([statistics.keys(), list(statistics['python'].values())])
-#     print(table_data)
-#     table = AsciiTable(table_data)
-#     print(table.table)
+def create_table(statistics, languages, title):
+    table_data = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата'
+    ]]
+    for language in languages:
+        line = [language]
+        for value in statistics[language].values():
+            line.append(value)
+        table_data.append(line)
 
+    table_instance = AsciiTable(table_data, title)
+    table_instance.justify_columns[2] = 'right'
+    print(table_instance.table)
 
 
 def main():
-    total_hh_statistics = {}
-    total_sj_statistics = {}
-    languages = ['python', 'java']
+    hh_statistics = {}
+    sj_statistics = {}
+    languages = ['c#', 'c++', 'ruby', 'javaScript', 'python', 'php', 'java']
+    load_dotenv()
+    sj_token = os.getenv('SJ_TOKEN')
 
     for language in languages:
-        #total_hh_statistics[language] = predict_rub_salary_hh(language)
-        total_sj_statistics[language] = predict_rub_salary_sj(language)
-        #create_table(total_sj_statistics)
+        hh_statistics[language] = predict_rub_salary_hh(language)
+        sj_statistics[language] = predict_rub_salary_sj(language, sj_token)
 
-
-
-    #print(total_hh_statistics)
-    print(total_sj_statistics)
-
-
+    create_table(sj_statistics, languages, 'SuperJob Moscow')
+    create_table(hh_statistics, languages, 'HeadHunter Moscow')
 
 
 if __name__ == '__main__':
